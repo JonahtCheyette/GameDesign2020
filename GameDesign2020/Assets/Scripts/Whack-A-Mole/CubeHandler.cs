c2a24138a763e7baad42dffe742ba7fcb6ff8024
Jonah <jonahcheyette@gmail.com>
2020-08-26 12:27:50 -0700

    I've got the (temp) moles popping up

---
+++ GameDesign2020/Assets/Scripts/Whack-A-Mole/CubeHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CubeHandler : MonoBehaviour {
+    [HideInInspector]
+    public GameObject[] cubes;
+
+    public int seed = 0;
+
+    System.Random rand;
+
+    void Start() {
+        cubes = new GameObject[gameObject.transform.childCount];
+        for (int i = 0; i < gameObject.transform.childCount; i++) {
+            cubes[i] = gameObject.transform.GetChild(i).gameObject;
+            cubes[i].SetActive(false);
+        }
+        rand = new System.Random(seed);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        for (int i = 0; i < cubes.Length; i++) {
+            if(rand.Next(0, 600) < 1) {
+                cubes[i].SetActive(true);
+            }
+        }
+    }
+}
---
+++ GameDesign2020/Assets/Scripts/Whack-A-Mole/MouseCube.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MouseCube : MonoBehaviour {
+    public Camera _camera;
+    public Transform cube;
+
+    private void Update() {
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+        cube.position = ray.GetPoint(50);
+    }
+}
